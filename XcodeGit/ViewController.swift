0c453df09eb3647f687dcc15c5743e6586b1bfaa
minjyun3011 <grand3canyon10@gmail.com>
2023-08-27 13:54:35 +0900

    紐づけ完了!!

--- XcodeGit/ViewController.swift
+++ XcodeGit/ViewController.swift
@@ -14,7 +14,8 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         print("初めてのコミット")
     }
-
+    @IBOutlet var spartabutton: UIView!
+    
 
 }
 
